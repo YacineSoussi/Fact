768819dcf2aae342dda7e5a67ee514e6345a1346
Yacine <Yacine1.soussi@gmail.com>
2020-04-26 11:19:11 +0200

    test deploiement

--- assets/js/pages/HomePage.jsx
+++ assets/js/pages/HomePage.jsx
@@ -5,7 +5,7 @@ const HomePage = props => {
     <div className="jumbotron">
       <h1 className="display-3">Bonjour à tous</h1>
       <p className="lead">
-        Voici Fact ! le site qui vous permet de gérer facilement vos clients et vos factures !
+        Bonjour voici Fact ! le site qui vous permet de gérer facilement vos clients et vos factures !
       </p>
       <hr className="my-4" />
       <p>
